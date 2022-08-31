85c8b44e5ca9ccf22fb0eb273fce3602fc6e4085
alex3 <alexprocards@mail.ru>
2022-08-31 19:12:33 +0500

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,75 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        container = Arrays.copyOf(container, container.length * 2);
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        T prev = get(index);
+        container[index] = newValue;
+        return prev;
+    }
+
+    @Override
+    public T remove(int index) {
+        T removes = get(index);
+        System.arraycopy(container, index + 1, container, index, size - index - 1);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return removes;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private final int expectedModCount = modCount;
+            private int count = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[count++];
+            }
+        };
+    }
+}
