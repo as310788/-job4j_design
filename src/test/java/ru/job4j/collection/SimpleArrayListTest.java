510d89171463f8816a3375e8fde1ae1663b9a851
alex3 <alexprocards@mail.ru>
2022-08-31 19:23:06 +0500

    1. Динамический список на массиве. [#158 #338333]

--- src/test/java/ru/job4j/collection/SimpleArrayListTest.java
+++ src/test/java/ru/job4j/collection/SimpleArrayListTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.NoSuchElementException;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
