9c32d43cb4c74239a5ba2eb352f46ec8c8cac7f6
alex3 <alexprocards@mail.ru>
2022-11-29 14:34:58 +0500

    1. Читаем файл конфигурации [#858 #363125]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -22,7 +22,17 @@ class ConfigTest {
     }
 
     @Test
-    void whenIOException() throws IllegalArgumentException {
+    public void whenPairWithNullFormat() throws IllegalArgumentException {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+        assertThat(config.value("surname")).isEqualTo(null);
+    }
+
+
+    @Test
+    void whenException() throws IllegalArgumentException {
         String path = "app.properties";
         Config config = new Config(path);
         assertThatThrownBy(config::load)
@@ -30,11 +40,18 @@ class ConfigTest {
     }
 
     @Test
-    public void whenPairWithNullFormat() throws IllegalArgumentException {
-        String path = "./data/pair_without_comment.properties";
+    void whenNotValues() throws IllegalArgumentException {
+        String path = "app.properties";
         Config config = new Config(path);
-        config.load();
-        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
-        assertThat(config.value("surname")).isEqualTo(null);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNotKey() throws IllegalArgumentException {
+        String path = "app.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
