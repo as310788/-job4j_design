e42ecd831b4f6fac4d5bed39f778e66ef5a6b712
alex3 <alexprocards@mail.ru>
2022-11-30 16:09:23 +0500

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileWriter(target))) {
+            boolean result = true;
+            while (in.ready()) {
+                String str = in.readLine();
+                String[] items = str.split("\\s");
+                String status = items[0];
+                String data = items[1];
+                if (("400".contains(status) || "500".contains(status)) && result) {
+                    out.print(data + ";");
+                    result = false;
+                }
+                if (("200".contains(status) || "300".contains(status)) && !result) {
+                    out.println(data + ";");
+                    result = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+public static void main(String[] args) {
+    Analysis analysis = new Analysis();
+    analysis.unavailable("source.txt", "target.txt");
+}
+}
