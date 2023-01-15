ce5083bcba1f589f40a53a90f23c051e71d4d091
alex3 <alexprocards@mail.ru>
2023-01-15 12:52:28 +0500

    5. Валидация параметров запуска. [#246865 #381035]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,16 +9,32 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException(
-                    "Здесь должно быть два параметра!");
-        }
+        validate(args);
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        search(start, p -> p.toFile().
+                getName().
+                endsWith(args[1])).
+                forEach(System.out::println);
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Здесь должно быть два параметра!");
+        }
+
+        if (!Files.exists(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Значение отсутствует в каталоге");
+        }
+        if (!Files.isDirectory(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Файл не является каталогом");
+        }
+        if (args[1].startsWith(".") && args[1].length() > 1) {
+            throw new IllegalArgumentException("Символ не является расширением");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles  searcher = new SearchFiles(condition);
+        SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
