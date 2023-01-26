231d3790bda363bfd8a14c778a9355ad2028a659
alex3 <alexprocards@mail.ru>
2023-01-26 16:59:29 +0500

    14. Архивировать проект [#861 #385216]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -33,12 +33,29 @@ public class Zip {
         }
     }
 
+    public static Exception validation(String[] args) throws IllegalArgumentException {
+        if (!args[0].contains("=")) {
+            throw new IllegalArgumentException("Первый аргумент не существует " + args[0]);
+        }
+        if (!args[1].contains("=")) {
+            throw new IllegalArgumentException("Второй аргумент не существует " + args[1]);
+        }
+        if (!args[2].contains("=")) {
+            throw new IllegalArgumentException("Третий аргумент не существует " + args[2]);
+        }
+        if (!Paths.get(args[0].substring(args[0].indexOf("=") + 1)).toFile().exists()) {
+            throw new IllegalArgumentException("Корневая папка не существует " + args[0]);
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
+        validation(args);
         ArgsName argsName = ArgsName.of(args);
         Path mainSource = Paths.get(argsName.get("d"));
         List<Path> sources = Search.search(mainSource, p -> !p.toFile().getName().endsWith(argsName.get("e")));
