13f3b4d4519bfea75b0d8ab0616da97776416c2b
alex3 <alexprocards@mail.ru>
2023-01-27 18:53:29 +0500

    14. Архивировать проект [#861 #385216]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -33,29 +33,7 @@ public class Zip {
         }
     }
 
-    public static Exception validation(String[] args) throws IllegalArgumentException {
-        if (!args[0].contains("=")) {
-            throw new IllegalArgumentException("Первый аргумент не существует " + args[0]);
-        }
-        if (!args[1].contains("=")) {
-            throw new IllegalArgumentException("Второй аргумент не существует " + args[1]);
-        }
-        if (!args[2].contains("=")) {
-            throw new IllegalArgumentException("Третий аргумент не существует " + args[2]);
-        }
-        if (!Paths.get(args[0].substring(args[0].indexOf("=") + 1)).toFile().exists()) {
-            throw new IllegalArgumentException("Корневая папка не существует " + args[0]);
-        }
-        return null;
-    }
-
-    public static void main(String[] args) throws IOException {
-        Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
-        validation(args);
+    private static void validation(String[] args) throws IllegalArgumentException, IOException {
         ArgsName argsName = ArgsName.of(args);
         Path mainSource = Paths.get(argsName.get("d"));
         List<Path> sources = Search.search(mainSource, p -> !p.toFile().getName().endsWith(argsName.get("e")));
@@ -63,5 +41,17 @@ public class Zip {
                 sources,
                 new File(argsName.get("o"))
         );
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Недостаточно аргументов");
+        }
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+        validation(args);
     }
 }
