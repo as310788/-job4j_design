29fc1199dacb60bb6e6530382308cb9c5a405f98
alex3 <alexprocards@mail.ru>
2023-01-29 12:51:48 +0500

    14. Архивировать проект[#861 #385216]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,13 +1,20 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+
 public class Zip {
+    private static Path directory;
+    private static String exclude;
+    private static Path output;
+
 
     public static void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
@@ -33,25 +40,49 @@ public class Zip {
         }
     }
 
-    private static void validation(String[] args) throws IllegalArgumentException, IOException {
+    private static List<File> searching(String[] args) throws IOException {
+        Search search = new Search();
+        return search.search(Paths.get(ArgsName.of(args).get("d")),
+                        f -> !f.toFile()
+                                .getName()
+                                .endsWith(ArgsName.of(args).get("e")))
+                .stream()
+                .map(p -> p.toFile())
+                .collect(Collectors.toList());
+    }
+
+    private static void validation(String[] args)throws IllegalArgumentException {
         ArgsName argsName = ArgsName.of(args);
-        Path mainSource = Paths.get(argsName.get("d"));
-        List<Path> sources = Search.search(mainSource, p -> !p.toFile().getName().endsWith(argsName.get("e")));
-        packFiles(
-                sources,
-                new File(argsName.get("o"))
-        );
+        String d = argsName.get("d");
+        String e = argsName.get("e");
+        String o = argsName.get("o");
+        directory = Path.of(d);
+        if (!Files.exists(directory)) {
+            throw new IllegalArgumentException("Каталог не существует");
+        }
+        if (!e.contains(".") || e.endsWith(".") || !o.endsWith(".zip")) {
+            throw new IllegalArgumentException("Недопустимый формат файла");
+        }
+        exclude = e.substring(e.indexOf("."));
+        output = Path.of(o);
     }
 
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             throw new IllegalArgumentException("Недостаточно аргументов");
         }
+        List<File> list;
+        try {
+            validation(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        list = searching(args);
+        list.forEach(System.out::println);
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
-        validation(args);
     }
 }
