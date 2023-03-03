f7dac990fc93236797797eb27c04f3b230854826
alex3 <alexprocards@mail.ru>
2023-03-03 16:09:46 +0500

    17. Scanner [#504791 #393762]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,64 +1,67 @@
 package ru.job4j.io;
 
-
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class CSVReader {
-
-    public static List<Integer> filter(List<String> list) {
-        List<Integer> result = new ArrayList<>();
-        for (String s : list) {
-            if ("name".equals(s)) {
-                result.add(0);
-            }
-            if ("age".equals(s)) {
-                result.add(1);
-            }
-            if ("last_name".equals(s)) {
-                result.add(2);
-            }
-            if ("education".equals(s)) {
-                result.add(3);
-            }
-        }
-        return result;
-    }
-
-    public static void handle(ArgsName argsName) throws Exception {
-        List<String> filter = List.of(argsName.get("filter").split(","));
-        List<Integer> result = filter(filter);
-        String delimiter = argsName.get("delimiter");
-        try (Scanner scanner =
-                     new Scanner(new File(argsName.get("path")));
-             PrintWriter out = new PrintWriter(new FileOutputStream(argsName.get("out")))) {
+    public static void handle(ArgsName argsName) {
+        var file = Path.of(argsName.get("path")).toFile();
+        var target = "stdout".equals(argsName.get("out"))
+                ? Path.of("plug.csv")
+                : Path.of(argsName.get("out"));
+        Integer[] indexOf = getIndexes(argsName, file);
+        try (PrintWriter print = new PrintWriter(target.toFile());
+             Scanner scanner = new Scanner(file)) {
+            StringBuilder string = new StringBuilder();
             while (scanner.hasNext()) {
-                String[] temp = scanner.nextLine().split(delimiter);
-                String s = "";
-                for (int i = 0; i < result.size(); i++) {
-                    if (i == result.size() - 1) {
-                        s = s + temp[result.get(i)];
-                        break;
+                String s = scanner.nextLine();
+                String[] spl = s.split(argsName.get("delimiter"));
+                String[] inOrder = new String[spl.length];
+                for (int i = 0; i < spl.length; i++) {
+                    if (indexOf[i] >= 0) {
+                        inOrder[indexOf[i]] = spl[i];
                     }
-                    s = s + temp[result.get(i)] + ";";
                 }
-                writer(s, argsName, out);
+                for (var str : inOrder) {
+                    if (str != null) {
+                        string.append(str).append(";");
+                    }
+                }
+                String toPrint = string.substring(0, string.length() - 1);
+                if (Path.of("plug.csv").equals(target)) {
+                    System.out.println(toPrint);
+                } else {
+                    print.println(toPrint);
+                }
+                string = new StringBuilder();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void writer(String s, ArgsName argsName, PrintWriter out) {
-        String value = argsName.get("out");
-        if ("stdout".equals(value)) {
-            System.out.println(s);
-        } else {
-            out.println(s);
+    private static Integer[] getIndexes(ArgsName argsName, File file) {
+        Integer[] rsl = new Integer[0];
+        try (Scanner scanner = new Scanner(file)) {
+            String[] fields = scanner.nextLine().split(argsName.get("delimiter"));
+            List<String> filters = List.of(argsName.get("filter").split(","));
+            Integer[] indexes = new Integer[fields.length];
+            for (int i = 0; i < fields.length; i++) {
+                if (filters.contains(fields[i])) {
+                    indexes[i] = filters.indexOf(fields[i]);
+                } else {
+                    indexes[i] = -1;
+                }
+            }
+            rsl = indexes;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return rsl;
     }
 
     public static void validate(ArgsName argsName) {
@@ -76,7 +79,7 @@ public class CSVReader {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("args.length != 4");
         }
